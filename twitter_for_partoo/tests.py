b222cbfa8ecc4e1078ed21bb47d7f7e0ccdd7c7c
Bencodev <benoit.cotte@essec.edu>
2016-11-02 11:02:17 +0100

    add tests for newtweet

--- twitter_for_partoo/tests.py
+++ twitter_for_partoo/tests.py
@@ -1,4 +1,5 @@
 import unittest
+import mock
 
 from pyramid import testing
 
@@ -16,6 +17,17 @@ class ViewTests(unittest.TestCase):
         info = my_view(request)
         self.assertEqual(info['project'], 'twitter_for_partoo')
 
+    def test_newtweet(self):
+        from .views import newtweet, api
+        from pyramid.httpexceptions import HTTPSeeOther
+
+        with mock.patch.object(api, "request") as mock_api_request:
+            request = testing.DummyRequest()
+
+            with self.assertRaises(HTTPSeeOther):
+                _ = newtweet(request)
+                mock_api_request.assert_called_with('statuses/update', {'status': mock.ANY})
+
 
 class FunctionalTests(unittest.TestCase):
     def setUp(self):
@@ -27,3 +39,10 @@ class FunctionalTests(unittest.TestCase):
     def test_root(self):
         res = self.testapp.get('/', status=200)
         self.assertTrue(b'Pyramid' in res.body)
+
+    def test_newtweet(self):
+        from .views import api
+        with mock.patch.object(api, "request") as mock_api_request:
+            res = self.testapp.post('/newtweet', status=303)
+            mock_api_request.assert_called_with('statuses/update', {'status': mock.ANY})
+            self.assertTrue(res.status_code, 303)
